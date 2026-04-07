5eb198eb120fa4d7686b1f3600e25aad7ad362b4
Nathi774-dev <adamschad589@gmail.com>
2026-04-07 17:32:14 +0000

    These are the final staged finance webapp commits before deployment

---
+++ utils/sendMail.js
@@ -0,0 +1,19 @@
+const nodemailer = require("nodemailer")
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.PASS
+    }
+})
+
+const sendMail = async (to, subject, text) => {
+    await transporter.sendMail({
+        to,
+        subject,
+        text
+    })
+}
+
+module.exports = sendMail;
