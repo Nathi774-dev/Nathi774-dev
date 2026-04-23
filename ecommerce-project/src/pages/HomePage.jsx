fa66eb5c65d1f4567c3d4fe1985ed2859b1bd8ee
Nathi774-dev <adamschad589@gmail.com>
2026-04-23 18:10:28 +0200

    Added some starter code for the home page and index file

---
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+export function HomePage() {
+    return (
+        <>
+            <div className="header">
+                <div className="left-section">
+                    <a href="index.html" className="header-link">
+                        <img className="logo" src="assets/images/logo-white.png" />
+                        <img className="mobile-logo" src="assets/images/mobile-logo-white.png" />
+                    </a>
+                </div>
+                <div className="middle-section"></div>
+            </div>
+            <div className="home-page"></div>
+        </>
+    );
+}
